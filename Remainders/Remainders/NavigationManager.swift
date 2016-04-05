667461e2219bd2b5b6f0560bdf041579cd6fab2c
pittNearsoft <projas@nearsoft.com>
2016-04-05 16:24:45 -0700

    Add NavigationManager to choose which view controller would be the root

---
+++ Remainders/Remainders/NavigationManager.swift
@@ -0,0 +1,36 @@
+//
+//  NavigationManager.swift
+//  Remainders
+//
+//  Created by projas on 4/5/16.
+//  Copyright © 2016 projas. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class NavigationManager: NSObject {
+    
+    static func isAnyUserLogged() -> Bool{
+
+        let realm = try! Realm()
+        
+        let user = realm.objects(User).filter("isLogged == true")
+        if user.count >= 1{
+            return true
+        }
+        
+        return false
+    }
+    
+    static func goToStoryboard(storyboardName: String, viewControllerId: String) -> Void{
+        let app = UIApplication.sharedApplication().delegate!
+        
+        let storyboard: UIStoryboard = UIStoryboard(name: storyboardName, bundle: nil)
+        let vc = storyboard.instantiateViewControllerWithIdentifier(viewControllerId)
+        app.window?!.rootViewController = vc
+        
+        UIView.transitionWithView(app.window!!, duration: 0.5, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { app.window?!.rootViewController = vc}, completion: nil)
+    }
+
+}

dc2e7326c86a84e1a753916772f076c4abcdba25
pittNearsoft <projas@nearsoft.com>
2016-04-06 15:03:47 -0700

    Add "AddEventViewController" class

---
+++ Remainders/Remainders/AddEventViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AddEventViewController.swift
+//  Remainders
+//
+//  Created by projas on 4/6/16.
+//  Copyright © 2016 projas. All rights reserved.
+//
+
+import UIKit
+
+class AddEventViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func closeView(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

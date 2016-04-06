5380100ec0c803aa52ce00f298ff37ebd4264259
pittNearsoft <projas@nearsoft.com>
2016-04-06 14:34:21 -0700

    Add EventViewCell class for custom cells

---
+++ Remainders/Remainders/EventViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventViewCell.swift
+//  Remainders
+//
+//  Created by projas on 4/6/16.
+//  Copyright © 2016 projas. All rights reserved.
+//
+
+import UIKit
+
+class EventViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

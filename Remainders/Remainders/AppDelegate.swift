f6be6731ebcb8edf1e5f39dbf1a8c6581010f03a
pittNearsoft <projas@nearsoft.com>
2016-04-06 11:59:38 -0700

    Add setRealmSchemaWithVersion on AppDelegate

--- Remainders/Remainders/AppDelegate.swift
+++ Remainders/Remainders/AppDelegate.swift
@@ -18,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
+        UserViewModel.setRealmSchemaWithVersion(2)
+        
+        
         if UserViewModel.isAnyUserLogged(){
             NavigationManager.goToStoryboard("Main", viewControllerId: "MainViewController")
         }else{

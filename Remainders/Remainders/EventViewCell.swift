3ed45385e1747030efd4229cfb0bdd3ed138be25
pittNearsoft <projas@nearsoft.com>
2016-04-07 15:59:11 -0700

    Add EventViewCell by mistake again

--- Remainders/Remainders/EventViewCell.swift
+++ Remainders/Remainders/EventViewCell.swift
@@ -2,7 +2,7 @@
 //  EventViewCell.swift
 //  Remainders
 //
-//  Created by projas on 4/6/16.
+//  Created by projas on 4/7/16.
 //  Copyright © 2016 projas. All rights reserved.
 //
 
@@ -10,14 +10,9 @@ import UIKit
 
 class EventViewCell: UITableViewCell {
 
-    
-    @IBOutlet weak var calendarView: UIView!
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        self.calendarView.layer.borderWidth = 1
-        self.calendarView.layer.borderColor = UIColor.blackColor().CGColor
     }
 
     override func setSelected(selected: Bool, animated: Bool) {

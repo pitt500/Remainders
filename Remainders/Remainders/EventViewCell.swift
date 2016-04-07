327816f71e6832291afdaaede31827fb6669560e
pittNearsoft <projas@nearsoft.com>
2016-04-07 16:03:52 -0700

    Add configureCellWithEvent method

--- Remainders/Remainders/EventViewCell.swift
+++ Remainders/Remainders/EventViewCell.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class EventViewCell: UITableViewCell {
 
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var monthLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        dayLabel.layer.borderWidth = 1
+        dayLabel.layer.borderColor = UIColor.blackColor().CGColor
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func configureCellWithEvent(event: Event) -> Void {
+        self.titleLabel.text = event.title
+        self.dayLabel.text = NSCalendar.getDayFromDate(event.date)
+        self.monthLabel.text = NSCalendar.getMonthNameFromDate(event.date)
     }
 
 }

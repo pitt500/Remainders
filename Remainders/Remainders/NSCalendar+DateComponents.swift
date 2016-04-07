9a4b146531eb874135f3f52bde5984cb3bb4ed59
pittNearsoft <projas@nearsoft.com>
2016-04-07 16:00:54 -0700

    Add DateComponents extension to get parts of any date (day or month so far)

---
+++ Remainders/Remainders/NSCalendar+DateComponents.swift
@@ -0,0 +1,28 @@
+//
+//  NSCalendar+DateComponents.swift
+//  Remainders
+//
+//  Created by projas on 4/7/16.
+//  Copyright © 2016 projas. All rights reserved.
+//
+
+import UIKit
+
+extension NSCalendar {
+
+    static func getMonthNameFromDate(date: NSDate) -> String{
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([.Day , .Month , .Year], fromDate: date)
+        
+        let dateFormatter: NSDateFormatter = NSDateFormatter()
+        let months = dateFormatter.shortMonthSymbols
+        return months[components.month-1]
+    }
+    
+    
+    static func getDayFromDate(date: NSDate) -> String{
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([.Day , .Month , .Year], fromDate: date)
+        return String(components.day)
+    }
+}

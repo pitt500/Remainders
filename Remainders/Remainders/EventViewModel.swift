2ae98f5d55f6a44bd20c45f601a27462251a3c98
pittNearsoft <projas@nearsoft.com>
2016-04-07 11:45:25 -0700

    Add EventViewModel class for business layer

---
+++ Remainders/Remainders/EventViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  EventViewModel.swift
+//  Remainders
+//
+//  Created by projas on 4/7/16.
+//  Copyright © 2016 projas. All rights reserved.
+//
+
+import UIKit
+
+class EventViewModel: NSObject {
+
+}

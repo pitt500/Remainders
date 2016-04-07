2ea65aac4487a2a27ef20023c7e88a1c24e44220
pittNearsoft <projas@nearsoft.com>
2016-04-06 22:40:54 -0700

    Update SchemaVersion from 2 to 3

--- Remainders/Remainders/AppDelegate.swift
+++ Remainders/Remainders/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        UserViewModel.setRealmSchemaWithVersion(2)
+        UserViewModel.setRealmSchemaWithVersion(3)
         
         
         if UserViewModel.isAnyUserLogged(){

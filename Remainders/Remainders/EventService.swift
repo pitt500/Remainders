a5cfc7118bac8959752e50c02adff5df283a31d9
pittNearsoft <projas@nearsoft.com>
2016-04-13 16:18:00 -0700

    Remove unnecesary comments in Service Layer

--- Remainders/Remainders/EventService.swift
+++ Remainders/Remainders/EventService.swift
@@ -13,145 +13,122 @@ class EventService: NSObject {
     
     static func getEventsForCurrentUser(dateComparison: DateComparison, completion: ((events: [Event]) -> Void)?, onFailure: ((error: NSError) -> Void)?) -> Void{
         
-//        let queue = dispatch_queue_create("getEventsForCurrentUser", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(queue) {
-            do{
-                try UserService.getLoggedUserWithCompletionHandler({ (user) in
-                    let today = NSDate()
-                    let eventArray = Array(user.events)
-                    let filteredEvents: [Event]
-                    
-                    if dateComparison == .PastEvents {
-                        filteredEvents = eventArray.filter({ $0.date < today }).sort({ $0.date > $1.date })
-                    } else if dateComparison == .UpcomingEvents{
-                        filteredEvents =  eventArray.filter({ $0.date > today }).sort({ $0.date > $1.date })
-                    }else{
-                        //dateComparison == .Today
-                        filteredEvents =  eventArray.filter({ $0.date == today }).sort({ $0.date > $1.date })
-                    }
-                    
-                    completion?(events: filteredEvents)
-                }, onFailure: { (error) in
-                    onFailure?(error: error)
-                })
-            }catch{
-                onFailure?(error: NSError(domain: "", code: 404, userInfo: nil))
-            }
-//        }
+        do{
+            try UserService.getLoggedUserWithCompletionHandler({ (user) in
+                let today = NSDate()
+                let eventArray = Array(user.events)
+                let filteredEvents: [Event]
+                
+                if dateComparison == .PastEvents {
+                    filteredEvents = eventArray.filter({ $0.date < today }).sort({ $0.date > $1.date })
+                } else if dateComparison == .UpcomingEvents{
+                    filteredEvents =  eventArray.filter({ $0.date > today }).sort({ $0.date > $1.date })
+                }else{
+                    //dateComparison == .Today
+                    filteredEvents =  eventArray.filter({ $0.date == today }).sort({ $0.date > $1.date })
+                }
+                
+                completion?(events: filteredEvents)
+            }, onFailure: { (error) in
+                onFailure?(error: error)
+            })
+        }catch{
+            onFailure?(error: NSError(domain: "", code: 404, userInfo: nil))
+        }
         
-    
     }
     
     
     static func getEventWithDate(date: NSDate, completion: ((event: Event) -> Void)?, onFailure: ((error: NSError) -> Void)?){
-        
-//        let queue = dispatch_queue_create("getEventWithDate", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(queue) {
-            do{
-                try UserService.getLoggedUserWithCompletionHandler({ (user) in
-                    let event = user.events.filter("date == %@",date).first!
-                    completion!(event: event)
-                }, onFailure: { (error) in
-                    onFailure?(error: error)
-                })
-            }catch{
-                onFailure?(error: NSError(domain: "", code: 404, userInfo: nil))
-            }
-//        }
-        
+    
+        do{
+            try UserService.getLoggedUserWithCompletionHandler({ (user) in
+                let event = user.events.filter("date == %@",date).first!
+                completion!(event: event)
+            }, onFailure: { (error) in
+                onFailure?(error: error)
+            })
+        }catch{
+            onFailure?(error: NSError(domain: "", code: 404, userInfo: nil))
+        }
     }
     
     static func saveEventIntoRealm(event: Event, completion: (() -> Void)?,  onFailure: ((error: NSError) -> Void)?) throws -> Void {
-//        let queue = dispatch_queue_create("saveEventIntoRealm", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(queue) {
-            do{
-                let realm = try Realm()
-                
-                try realm.write({
-                    realm.add(event)
-                })
-                completion?()
-            }catch{
-                onFailure?(error: NSError(domain: "", code: 404, userInfo: nil))
-            }
-//        }
+        do{
+            let realm = try Realm()
+            
+            try realm.write({
+                realm.add(event)
+            })
+            completion?()
+        }catch{
+            onFailure?(error: NSError(domain: "", code: 404, userInfo: nil))
+        }
     }
     
     static func saveUserEventIntoRealm(event: Event,completion: (() -> Void)?,  onFailure: ((error: NSError) -> Void)?) throws -> Void{
 
-//        let queue = dispatch_queue_create("saveUserEventIntoRealm", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(queue) {
-            do{
-                try UserService.getLoggedUserWithCompletionHandler({ (user) in
-                    let realm = try! Realm()
-                    
-                    try! realm.write({
-                        user.events.append(event)
-                    })
-                }, onFailure: { (error) in
-                    onFailure?(error: error)
+        do{
+            try UserService.getLoggedUserWithCompletionHandler({ (user) in
+                let realm = try! Realm()
+                
+                try! realm.write({
+                    user.events.append(event)
                 })
-            }catch{
-                onFailure?(error: NSError(domain: "", code: 404, userInfo: nil))
-            }
-//        }
+            }, onFailure: { (error) in
+                onFailure?(error: error)
+            })
+        }catch{
+            onFailure?(error: NSError(domain: "", code: 404, userInfo: nil))
+        }
 
     }
     
     
     static func saveNewEvent(event: Event, completion: (() -> Void)?,  onFailure: ((error: NSError) -> Void)?){
-//        let queue = dispatch_queue_create("saveNewEvent", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(queue) {
-            do{
-                try saveEventIntoRealm(event, completion: nil, onFailure: nil)
-                try saveUserEventIntoRealm(event, completion: nil, onFailure: nil)
-                try UILocalNotification.setNotificationWithEvent(event)
-                completion?()
-            }catch{
-                onFailure?(error: NSError(domain: "", code: 404, userInfo: nil))
-            }
-//        }
+        do{
+            try saveEventIntoRealm(event, completion: nil, onFailure: nil)
+            try saveUserEventIntoRealm(event, completion: nil, onFailure: nil)
+            try UILocalNotification.setNotificationWithEvent(event)
+            completion?()
+        }catch{
+            onFailure?(error: NSError(domain: "", code: 404, userInfo: nil))
+        }
     }
     
     
     static func updateEvent(eventToUpdate: Event, newEvent: Event, completion: (() -> Void)?,  onFailure: ((error: NSError) -> Void)?){
         
-//        let queue = dispatch_queue_create("updateEvent", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(queue) {
-            do{
-                let realm = try Realm()
-                
-                try realm.write({
-                    eventToUpdate.title = newEvent.title
-                    eventToUpdate.date = newEvent.date
-                    eventToUpdate.note = newEvent.note
-                })
-                try UILocalNotification.setNotificationWithEvent(eventToUpdate)
-                
-                completion?()
-            }catch{
-                onFailure?(error: NSError(domain: "", code: 404, userInfo: nil))
-            }
-//        }
+        do{
+            let realm = try Realm()
+            
+            try realm.write({
+                eventToUpdate.title = newEvent.title
+                eventToUpdate.date = newEvent.date
+                eventToUpdate.note = newEvent.note
+            })
+            try UILocalNotification.setNotificationWithEvent(eventToUpdate)
+            
+            completion?()
+        }catch{
+            onFailure?(error: NSError(domain: "", code: 404, userInfo: nil))
+        }
     }
     
     static func deleteEvent(event: Event, completion: (() -> Void)?,  onFailure: ((error: NSError) -> Void)?){
         
-//        let queue = dispatch_queue_create("deleteEvent", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(queue) {
-            do{
-                try UILocalNotification.cancelNotificationForEvent(event)
+        do{
+            try UILocalNotification.cancelNotificationForEvent(event)
+            
+            let realm = try Realm()
+            try realm.write({
+                realm.delete(event)
                 
-                let realm = try Realm()
-                try realm.write({
-                    realm.delete(event)
-                    
-                })
-                
-                completion?()
-            }catch{
-                onFailure?(error: NSError(domain: "", code: 404, userInfo: nil))
-            }
-//        }
+            })
+            
+            completion?()
+        }catch{
+            onFailure?(error: NSError(domain: "", code: 404, userInfo: nil))
+        }
     }
 }
--- Remainders/Remainders/UserService.swift
+++ Remainders/Remainders/UserService.swift
@@ -11,42 +11,33 @@ import RealmSwift
 
 class UserService: NSObject {
     static func getLoggedUserWithCompletionHandler(completion: ((user: User) ->Void)?, onFailure: ((error: NSError) -> Void)?) throws -> Void {
-        
-//        let queue = dispatch_queue_create("getLoggedUser", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(queue) {
-                do{
-                    let realm = try Realm()
-                    let user = realm.objects(User).filter("isLogged == true").first
-                    
-                    
-                    if let actualUser = user {
-                        completion?(user: actualUser)
-                    }else{
-                        onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
-                    }
-                    
-                }catch{
-                    onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
-                }
+        do{
+            let realm = try Realm()
+            let user = realm.objects(User).filter("isLogged == true").first
             
-//        }
+            
+            if let actualUser = user {
+                completion?(user: actualUser)
+            }else{
+                onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
+            }
+            
+        }catch{
+            onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
+        }
         
     }
     
     static func checkIfAnyUserIsLoggedWithCompletion(completion: ((isUserLogged: Bool) -> Void)?, onFailure: ((error: NSError) -> Void)?) -> Void{
         
-//        let queue = dispatch_queue_create("checkIfAnyUserIsLogged", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(queue) {
-            do{
-                let realm = try Realm()
-                let user = realm.objects(User).filter("isLogged == true")
-                let isAnyLogged = (user.count == 1) ? true : false
-                completion?(isUserLogged: isAnyLogged)
-            }catch{
-                onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
-            }
-//        }
-        
+        do{
+            let realm = try Realm()
+            let user = realm.objects(User).filter("isLogged == true")
+            let isAnyLogged = (user.count == 1) ? true : false
+            completion?(isUserLogged: isAnyLogged)
+        }catch{
+            onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
+        }
     }
     
     
@@ -55,88 +46,73 @@ class UserService: NSObject {
     
     static func getUserWithEmail(email: String, completion: ((user: User?) -> Void)?, onFailure: ((error: NSError) -> Void)?) -> Void{
         
-//        let queue = dispatch_queue_create("getUserWithEmail", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(queue) {
-            do{
-                let realm = try Realm()
-                let user: User? = realm.objects(User).filter("email == '\(email)'").first
-                completion?(user: user)
-            }catch{
-                onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
-            }
-//        }
+        do{
+            let realm = try Realm()
+            let user: User? = realm.objects(User).filter("email == '\(email)'").first
+            completion?(user: user)
+        }catch{
+            onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
+        }
     }
     
     
     static func deleteUserFromRealm(user: User, completion: (() -> Void)?, onFailure: ((error: NSError) -> Void)?) -> Void {
-//        let queue = dispatch_queue_create("deleteUserFromRealm", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(queue) {
-            do{
-                let realm = try Realm()
-                try realm.write {
-                    realm.delete(user)
-                }
-                completion?()
-            
-            }catch{
-                onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
+        do{
+            let realm = try Realm()
+            try realm.write {
+                realm.delete(user)
             }
-//        }
+            completion?()
+        
+        }catch{
+            onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
+        }
     }
     
     static func saveUserIntoRealm(user: User, completion: (() -> Void)?, onFailure: ((error: NSError) -> Void)?) -> Void {
         
-//        let queue = dispatch_queue_create("saveUserIntoRealm", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(queue) {
-            do{
-                let realm = try Realm()
-                
-                try realm.write({
-                    realm.add(user)
-                })
-                completion?()
-            }catch{
-                onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
-            }
-//        }
+        do{
+            let realm = try Realm()
+            
+            try realm.write({
+                realm.add(user)
+            })
+            completion?()
+        }catch{
+            onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
+        }
     }
     
     static func saveUserEventIntoRealm(event: Event, completion: (() -> Void)?, onFailure: ((error: NSError) -> Void)?) -> Void{
         
-//        let queue = dispatch_queue_create("saveUserEventIntoRealm", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(queue) {
-            do{
-                try getLoggedUserWithCompletionHandler({ (user) in
-                    let realm = try! Realm()
-                    
-                    try! realm.write({
-                        user.events.append(event)
-                    })
-                    completion?()
-                }) { (error) in
-                    onFailure?(error: error)
-                }
-            }catch{
-                onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
+        do{
+            try getLoggedUserWithCompletionHandler({ (user) in
+                let realm = try! Realm()
+                
+                try! realm.write({
+                    user.events.append(event)
+                })
+                completion?()
+            }) { (error) in
+                onFailure?(error: error)
             }
-//        }
+        }catch{
+            onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
+        }
     }
     
     static func updateLoginState(user: User, isUserLogged: Bool, completion: (() -> Void)?, onFailure: ((error: NSError) -> Void)?) -> Void {
         
-//        let queue = dispatch_queue_create("updateLoginState", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(queue) {
-            do{
-                let realm = try Realm()
-                
-                try realm.write({
-                    user.isLogged = isUserLogged
-                    realm.add(user, update: true)
-                })
-            }catch{
-                onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
-            }
-//        }
+        do{
+            let realm = try Realm()
+            
+            try realm.write({
+                user.isLogged = isUserLogged
+                realm.add(user, update: true)
+            })
+        }catch{
+            onFailure?(error: NSError(domain: "", code: 0, userInfo: ["message" : "Something went wrong"]))
+        }
     }
     
 }
